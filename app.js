168afb159a7a239eabfd5e7a2f254dd45e6b4d09
ereweA <149901988+ereweA@users.noreply.github.com>
2025-05-03 14:00:54 -0500

    dynamic product management

--- app.js
+++ app.js
@@ -11,5 +11,35 @@ iconCart.addEventListener('click', ()=>{
         cart.style.right == '-100%';
         container.style.transform = 'translateX(0)';
     }  
-    
 })
+close.addEventListener('click', ()=>{
+    cart.style.right = '-100%';
+    container.style.transform = 'translateX(0)';
+})
+
+let products = null;
+// get data from json file
+fetch('product.json')
+.then(response => response.json())
+.then(data => {
+    products = data;
+    addDataToHTML();
+})
+
+// show data in list in html
+function addDataToHTML(){
+    // remove default html data
+    let listProductHTML = document.querySelector('.listProduct');
+    listProductHTML.innerHTML = '';
+
+    // add new data
+    if(products != null){
+        products.forEach(product => {
+            let newProduct = document.createElement('div');
+            newProduct.classList.add('item');
+            newProduct.innerHTML =
+            '<img src="${product.image}" alt=""> <h2>${product.name}</h2> <div class="${product.price}">$50</div> <button onclick="addCart(${product.id})">Add to Cart</button> '
+            listProductHTML.appendChild(newProduct);
+        });
+    }
+}
